95f071f79b1afcbb864c8e516a9533760f145b94
Allen-Valdez <68507863+Allen-Valdez@users.noreply.github.com>
2020-11-23 23:50:39 -0800

    Final commit

---
+++ DogYears.js
@@ -0,0 +1,17 @@
+//My age
+let myAge = 22;
+//Early Years
+let earlyYears = 2;
+earlyYears *= 10.5;
+//This is taking 2 years off
+laterYears = myAge - 2;
+//Later Years
+laterYears *= 4;
+console.log(earlyYears);
+console.log(laterYears);
+
+myAgeInDogYears = earlyYears + laterYears;
+//Putting my name into lower case
+myName = 'Allen'.toLowerCase();
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
